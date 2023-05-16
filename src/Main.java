ed6e48adfb2118682ca826464a11c4373f921ed0
EgorYunev <egoryunev@vk.com>
2023-05-16 22:06:24 +0300

    Commit

--- src/Main.java
+++ src/Main.java
@@ -3,16 +3,20 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        String[] alphabet = {".-", "-...", ".--", "--.", "-..", ".", "...-", "--..", "..",
-                ".---", "-.-", ".-..", "--", "-.", "---", ".--.", ".-.", "...", "-", "..-", "..-.",
-                "....", "-.-.", "---.", "----", "--.-", ".--.-.", "-.--", "-..-", "...-...", "..--",
-                ".-.-",};
-        String[] russian = {"А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К", "Л", "М", "Н","О",
-                "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"};
-        String result = "";
         Scanner in = new Scanner(System.in);
         System.out.println("Введите текст на азбуке морза (. -), каждая буква разделена символом '/', без пробелов: ");
-        String input = in.next();
+        Decoder.decod(in.next());
+    }
+}
+class Decoder {
+    private static String[] alphabet = {".-", "-...", ".--", "--.", "-..", ".", "...-", "--..", "..",
+            ".---", "-.-", ".-..", "--", "-.", "---", ".--.", ".-.", "...", "-", "..-", "..-.",
+            "....", "-.-.", "---.", "----", "--.-", ".--.-.", "-.--", "-..-", "...-...", "..--",
+            ".-.-",};
+    private static String[] russian = {"А", "Б", "В", "Г", "Д", "Е", "Ж", "З", "И", "Й", "К", "Л", "М", "Н","О",
+            "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я"};
+    private static String result = "";
+    public static void decod(String input) {
         String[] parts = input.split("/");
         for (int i = 0; i < parts.length; i++){
             for (int g = 0; g < alphabet.length; g++){
@@ -22,6 +26,6 @@ public class Main {
             }
             result = result + " ";
         }
-        System.out.print(result);
+        System.out.println(result);;
     }
 }
